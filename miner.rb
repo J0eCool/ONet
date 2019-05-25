cd85591645dc5eeb4e7a9ea8b33cab83d4a31a89
J0eCool <count.j0ecool@gmail.com>
2019-05-24 19:54:04 -0700

    Track known servers, respond to HTTP GET w/ status dump

--- miner.rb
+++ miner.rb
@@ -1,11 +1,25 @@
 require "socket"
 
-def ping server, port
+class Server
+  attr_reader :ip
+  attr_reader :port
+
+  def initialize(ip, port)
+    @ip = ip
+    @port = port
+  end
+
+  def connect
+    TCPSocket.new(@ip, @port)
+  end
+end
+
+def ping(server)
   begin
-    socket = TCPSocket.new server, port
+    socket = server.connect
     socket.write "ping"
     resp = socket.recv 1024
-    puts "Pinged #{server}:#{port} - recvd response:\n#{resp}"
+    puts "Pinged #{server} - recvd response:\n#{resp}"
     return true
   rescue Exception => e
     puts "ping and fail w/ #{e}"
@@ -15,35 +29,69 @@ end
 
 START_PORT = 8000
 port = START_PORT
+known_servers = []
+server = nil
 loop do
-  exists = ping 'localhost', port
+  server = Server.new("localhost", port)
+  exists = ping server
   if exists
     port += 1
+    known_servers.push(server)
+    puts "Known nodes: #{known_servers}"
   else
     break
   end
 end
 
-server = TCPServer.new("localhost", port)
+tcp_server = TCPServer.new(server.ip, server.port)
 puts "Server started on #{port}"
 
-def connect(client)
+def connect(client, data)
   puts "New client: #{client}"
 
   request = client.readpartial 2048
   puts "Request:"
   puts request
   if request == "ping"
-    puts "oh hey dere we gots a peeng"
-    client.write "hi"
+    puts "Ping"
+    response = "hi"
+  elsif request.start_with?("GET")
+    puts "GET Request"
+    response = http_html_response(200, "<h1>Status</h1>#{escape_html(data[:known_servers].to_s)}")
   end
+  puts "Response:"
+  puts response
+  client.write response
 
   STDOUT.flush
   client.close
 end
 
+def http_html_response(code, body)
+  "HTTP/1.1 #{code}\r\n" +
+  "Content-type: text/html\r\n" +
+  "\r\n#{body}"
+end
+
+def escape_html(text)
+  result = ""
+  text.split('').each do |c|
+    if c == "<"
+      c = '&lt'
+    elsif c == ">"
+      c = '&gt'
+    end
+    result += c
+  end
+  result
+end
+
 loop do
   STDOUT.flush
-  client = server.accept
-  Thread.new { connect client }
+  client = tcp_server.accept
+  Thread.new do
+    connect(client, {
+      known_servers: known_servers
+    })
+  end
 end
